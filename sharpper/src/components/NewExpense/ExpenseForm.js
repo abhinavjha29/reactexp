7ccbe90829e40c377b9b7a4fe4e58ad19c909815
abhinavjha29 <abhinav_29@outlook.com>
2023-11-26 12:55:36 +0530

    added form function

--- sharpper/src/components/NewExpense/ExpenseForm.js
+++ sharpper/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
+  const [enteredTitle , setTitle] = useState('') ;
+  const [enteredAmount , setAmount] = useState('') ;
+  const [enteredDate , setDate] = useState('') ;
+  const changeTitle = (event)=>{
+    setTitle(event.target.value)
+  } 
+
+  const changeAmount = (event)=>{
+    setAmount(event.target.value)
+  }
+  const changeDate = (event)=>{
+    setDate(event.target.value)
+  }
+
+  const submitHandler =(e)=>{
+e.preventDefault() ;
+const expenseData = {
+  title : enteredTitle ,
+  amount : enteredAmount ,
+  date : new Date(enteredDate) 
+}
+console.log(expenseData) ;
+  }
+
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input onChange={changeTitle} type='text' />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input onChange={changeAmount} type='number' min='0.01' step='0.01' />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input onChange={changeDate} type='date' min='2019-01-01' max='2022-12-31' />
         </div>
       </div>
       <div className='new-expense__actions'>
